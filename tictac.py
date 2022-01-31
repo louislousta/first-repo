3d78fbafa43c646cd087f84eb7c2c0c2959328df
Louis Austen <louislousta@gmail.com>
2022-01-31 09:57:52 +0000

    added get player move function

--- tictac.py
+++ tictac.py
@@ -14,3 +14,30 @@ def render_board(board):
 init_board = new_board()
 render_board(init_board)
 
+def get_move(): #takes input in form tl/tm/etc and returns list index
+    move = input()
+    move_index = list
+    match move:
+        case 'tl':
+            move_index = [0,0] 
+        case 'tm':
+            move_index = [0,1]
+        case 'tr': 
+            move_index = [0,2]
+        case 'ml':
+            move_index = [1,0]
+        case 'mm':
+            move_index = [1,1]
+        case 'mr': 
+            move_index = [1,2]
+        case 'bl':
+            move_index = [2,0]
+        case 'bm': 
+            move_index = [2,1]
+        case 'br': 
+            move_index = [2,2]
+    return move_index
+
+while(True):
+    move = get_move()
+    print(move)
